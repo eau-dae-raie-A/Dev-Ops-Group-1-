9a02b4684287de2f90759286b7097afb00100317
moosey2003 <winminthaw20018@gmail.com>
2024-10-09 23:44:38 +0630

    32 query commits (fixed)

--- src/main/java/com/napier/devops/DatabaseService.java
+++ src/main/java/com/napier/devops/DatabaseService.java
@@ -540,14 +540,14 @@ public class DatabaseService {
 
     public void displayPopulationData(List<PopulationReport> dataList) {
         if (dataList != null && !dataList.isEmpty()) {
-            System.out.println("---------------------------------------------------------------------------------------------------------------------");
-            System.out.printf("%-36s | %-20s | %-20s | %-10s | %-20s | %-10s%n",
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%-40s | %-20s | %-20s | %-8s | %-20s | %-10s%n",
                     "Name", "Total Population", "City Population", "City %", "Non-City Population", "Non-City %");
-            System.out.println("---------------------------------------------------------------------------------------------------------------------");
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
 
             NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
             for (PopulationReport report : dataList) {
-                System.out.printf("%-36s | %-20s | %-20s | %-6.2f%% | %-20s | %-6.2f%%%n",
+                System.out.printf("%-40s | %-20s | %-20s | %-6.2f%% | %-20s | %-6.2f%%%n",
                         report.getName(),
                         numberFormat.format(report.getTotalPopulation()),
                         numberFormat.format(report.getCityPopulation()),
@@ -555,7 +555,7 @@ public class DatabaseService {
                         numberFormat.format(report.getNonCityPopulation()),
                         report.getNonCityPopulationPercentage());
             }
-            System.out.println("---------------------------------------------------------------------------------------------------------------------");
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
         } else {
             System.out.println("No data found.");
         }
